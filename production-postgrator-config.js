d11d905f93a5f4bbcdb591097194af629c765ada
zeidalidiez <zeidalidiez@gmail.com>
2021-01-28 04:11:45 -0800

    updates

--- production-postgrator-config.js
+++ production-postgrator-config.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 module.exports = {
   'migrationDirectory': 'migrations',
   'driver': 'pg',
-  'host': process.env.PRODUCTION_MIGRATION_DB_HOST,
-  'port': process.env.PRODUCTION_MIGRATION_DB_PORT,
-  'database': process.env.PRODUCTION_MIGRATION_DB_NAME,
-  'username': process.env.PRODUCTION_MIGRATION_DB_USER,
-  'password': process.env.PRODUCTION_MIGRATION_DB_PASS,
+  'host': process.env.MIGRATION_DB_HOST,
+  'port': process.env.MIGRATION_DB_PORT,
+  'database': process.env.MIGRATION_DB_NAME,
+  'username': process.env.MIGRATION_DB_USER,
+  'password': process.env.MIGRATION_DB_PASS,
   'ssl': true
 };
