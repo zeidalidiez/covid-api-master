40dcceb5fe32365b4a86909417ebfbadecd00dd3
zeidalidiez <zeidalidiez@gmail.com>
2021-01-29 13:17:31 -0800

    tests

---
+++ test/appointments.js
@@ -0,0 +1,10 @@
+const request = require("supertest")("https://coviddogwalking.herokuapp.com/covidapi");
+const expect = require("chai").expect;
+
+describe("GET /appointment", function () {
+  it("return error due to lack of authentication", async function () {
+    const response = await request.get("/appointment");
+
+    expect(response.status).to.eql(500);
+  });
+});
