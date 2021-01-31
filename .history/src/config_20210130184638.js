1b0d797e02d70dd0f88bd4e2b1b57ff634ab2655
Zeid Diez <69106534+zeidalidiez@users.noreply.github.com>
2021-01-30 20:20:21 -0800

    tests

---
+++ .history/src/config_20210130184638.js
@@ -0,0 +1,8 @@
+'use strict';
+module.exports = {
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/covidapi',
+  JWT_SECRET: process.env.JWT_SECRET || 'spaced-repetition-jwt-secret',
+
+};
